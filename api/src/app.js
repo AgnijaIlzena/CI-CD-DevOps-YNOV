511b1ca8cb4bc8abab9a89a18173b644bfd92600
AgnijaIlzena <ilzena.agnija@gmail.com>
2026-05-11 14:39:07 +0200

    upd routes

--- api/src/app.js
+++ api/src/app.js
@@ -131,4 +131,32 @@ app.post('/products', async (req, res) => {
   }
 });
 
+app.post('/orders', async (req, res) => {
+  // try {
+  //   const { name, description, price_cents, stock } = req.body;
+
+  //   if (!name || !description || !price_cents) {
+  //     return res.status(400).json({
+  //       error: 'name, description et price_cents sont obligatoires'
+  //     });
+  //   }
+
+  //   const result = await pool.query(
+  //     `INSERT INTO products (name, description, price_cents, stock)
+  //      VALUES ($1, $2, $3, $4)
+  //      RETURNING id, name, description, price_cents, stock`,
+  //     [name, description, price_cents, stock || 0]
+  //   );
+
+  //   res.status(201).json(result.rows[0]);
+  // } catch (error) {
+  //   res.status(500).json({
+  //     error: 'Impossible de créer le produit',
+  //     message: error.message
+  //   });
+  // }
+});
+
+app.post('/checkout', async (req, res) => {})
+
 module.exports = app;
